fec707f7a8075892c5b20fe234eb42f7f5c2b54e
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-11-24 12:12:40 +0300

    Delete Backend/src directory

--- Backend/src/Routes/email_route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { EmailController } = require("../Controllers/email_controller");
-
-router.get("/email", EmailController);
-
-module.exports = router;
