1c1d49c193f527313edb091f7cbcf265abcab790
Bz14 <bezueyerusalem@gmail.com>
2024-12-04 14:11:48 +0300

    Payment pages updatd.

--- Frontend/ecommerce/src/app/payment/payment-failure/page.tsx
+++ Frontend/ecommerce/src/app/payment/payment-failure/page.tsx
@@ -1,7 +1,26 @@
+import { FaTimesCircle } from "react-icons/fa";
+import Link from "next/link";
+
 const PaymentFailurePage = () => {
   return (
-    <div>
-      <h1>Payment Failed</h1>
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex justify-center p-20 bg-gray-100">
+        <div className="bg-white p-10 rounded-lg shadow-lg text-center">
+          <div className="text-4xl mb-4 flex gap-2 items-center justify-center text-primaryBrown ">
+            <FaTimesCircle className="text-red-500 text-6xl mb-4" />
+            <h1 className="text-3xl font-bold text-primaryDark mb-2">
+              Payment Failed
+            </h1>
+          </div>
+          <p className="text-lg text-gray-700 mb-6">
+            Unfortunately, your payment could not be processed. Please try
+            again.
+          </p>
+          <button className="bg-primaryBrown text-white px-6 py-2 rounded-lg shadow-md hover:bg-gray-500 transition duration-300">
+            <Link href="/payment">Retry Payment</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- Frontend/ecommerce/src/app/payment/payment-success/page.tsx
+++ Frontend/ecommerce/src/app/payment/payment-success/page.tsx
@@ -3,22 +3,24 @@ import Link from "next/link";
 
 const PaymentSuccessPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-10 rounded-lg shadow-lg text-center">
-        <div className="text-4xl mb-4 flex gap-2 items-center justify-center text-primaryBrown ">
-          <MdOutlineCelebration className="text-5xl " />
-          <h1>Congratulations</h1>
-        </div>
-        <h1 className="text-3xl font-bold text-primaryDark mb-2">
-          Payment Success
-        </h1>
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex justify-center p-20 bg-gray-100">
+        <div className="bg-white p-10 rounded-lg shadow-lg text-center">
+          <div className="text-4xl mb-4 flex gap-2 items-center justify-center text-primaryBrown ">
+            <MdOutlineCelebration className="text-5xl " />
+            <h1>Congratulations</h1>
+          </div>
+          <h1 className="text-3xl font-bold text-primaryDark mb-2">
+            Payment Success
+          </h1>
 
-        <p className="text-lg text-gray-700 mb-6">
-          Thank you for your purchase! Your payment was successful.
-        </p>
-        <button className="bg-primaryBrown text-white px-6 py-2 rounded-lg shadow-md hover:bg-gray-500 transition duration-300">
-          <Link href="/products">Continue Shopping</Link>
-        </button>
+          <p className="text-lg text-gray-700 mb-6">
+            Thank you for your purchase! Your payment was successful.
+          </p>
+          <button className="bg-primaryBrown text-white px-6 py-2 rounded-lg shadow-md hover:bg-gray-500 transition duration-300">
+            <Link href="/products">Continue Shopping</Link>
+          </button>
+        </div>
       </div>
     </div>
   );
